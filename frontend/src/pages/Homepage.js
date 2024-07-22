864453e135f051910a044d6c92387a496b2d372d
Jaswanth <madhajaswanth@gmail.com>
2024-07-22 01:23:48 -0500

    Final Commit

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Grid, Box, Button } from '@mui/material';
+import styled from 'styled-components';
+import Students from '../assets/students.jpg';
+import { LightPurpleButton } from '../components/buttonStyles';
+
+const Homepage = () => {
+  return (
+    <StyledContainer>
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={6}>
+          <img
+            src={Students}
+            alt="students"
+            style={{ width: '100%', margin: '10rem 2rem 0 0' }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <StyledPaper elevation={3}>
+            <StyledTitle>School Management System</StyledTitle>
+            <StyledText>A one stop destination for all students.</StyledText>
+            <StyledBox>
+              <StyledLink to="/choose">
+                <LightPurpleButton variant="contained" fullWidth>
+                  Login
+                </LightPurpleButton>
+              </StyledLink>
+              <StyledText>
+                Don't have an account?{' '}
+                <Link to="/Adminregister" style={{ color: '#550080' }}>
+                  Sign up
+                </Link>
+              </StyledText>
+            </StyledBox>
+          </StyledPaper>
+        </Grid>
+      </Grid>
+    </StyledContainer>
+  );
+};
+
+export default Homepage;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledPaper = styled.div`
+  padding: 24px;
+  height: 100vh;
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: center;
+  gap: 16px;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 2rem;
+  color: #121212;
+  /* font-family: "Manrope"; */
+  font-weight: bold;
+  padding-top: 0;
+  letter-spacing: normal;
+  line-height: -1;
+  margin-top: 15rem;
+`;
+
+const StyledText = styled.p`
+  /* color: #550080; */
+  margin-bottom: 30px;
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
